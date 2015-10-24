659d7cec11ad7ee21b5a469c57cdc03eb4f4fc19
manuelthieme <manuelthieme@googlemail.com>
2015-10-24 19:32:13 +0200

    escaping before querying

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $db = mysqli_connect($conf['db']['host'], $conf['db']['user'], $conf['db']['pass
 if ($db->connect_errno)
     echo "Failed to connect to MySQL: " . $mysqli->connect_error;
 
-$query = "SELECT * FROM ".$conf['db']['prefix']."count";
+$query = "SELECT * FROM ".$db->real_escape_string($conf['db']['prefix'])."count";
 $result = $db->query($query);
 
 $row = NULL;
@@ -21,20 +21,20 @@ if ($row) {
         $db->query("
             UPDATE ".$conf['db']['prefix']."count 
             SET count = count + 1
-            WHERE name = \"".$page."\"
+            WHERE name = \"".$db->real_escape_string($page)."\"
         ") or die("NONONO");
         header("Location: ".$conf['baseurl'].$page);
     }
-    $query = "SELECT count FROM ".$conf['db']['prefix']."count WHERE name = \"".$page."\"";
+    $query = "SELECT count FROM ".$db->real_escape_string($conf['db']['prefix'])."count WHERE name = \"".$page."\"";
     $result = $db->query($query);
     $count = $result->fetch_assoc()['count'];
     include('tpl/main.tpl');
 }
 else {
     $db->query("
-            INSERT INTO ".$conf['db']['prefix']."count 
+            INSERT INTO ".$db->real_escape_string($conf['db']['prefix'])."count 
             (name, count)
-            VALUES (\"".$page."\",1)
+            VALUES (\"".$db->real_escape_string($page)."\",1)
         ");
     header("Location: ".$conf['baseurl'].$page);
 }
